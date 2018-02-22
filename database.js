19c634657ee66aa13ee3470bcf56e593eed6e701
Estefania Loewenberg <estloewenberg@gmail.com>
2018-02-22 17:04:33 +0100

    updates petition

--- database.js
+++ database.js
@@ -1,17 +1,19 @@
-//run db.query(query, params) and pass in the query and the params and return db.query
-//db.query returns a promise so when i run insertNewSignature i can chain a .then()
-//and inside here i redirect to the thanks route in my post request.
-//module.exports to export the functions
-//in index.js require /database.js
-// then run the function
-
 var spicedPg = require("spiced-pg");
 
-var db = spicedPg("postgres:spicedling:password@localhost:5432/signatures");
+var db = spicedPg("postgres:postgres:postgres@localhost:5432/petition");
 
 exports.insertNewSignature = function(firstname, lastname, signature) {
-    return db.query(
-        "INSERT INTO signatures (first_name, last_name, signature) VALUES ($1, $2, $3)",
-        [firstname, lastname, signature]
-    );
+    return db
+        .query(
+            "INSERT INTO signatures (first_name, last_name, signature) VALUES ($1, $2, $3) RETURNING id",
+            [firstname, lastname, signature]
+        )
+        .then(function(results) {
+            console.log("this is results new sig", results.rows);
+            return results.rows[0].id;
+        });
+};
+
+exports.signedSession = function(id) {
+    return db.query("SELECT id FROM signatures VALUES (id = $1)", [id]);
 };
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
 const handlebars = require("express-handlebars");
 const fs = require("fs");
 const { insertNewSignature } = require("./database");
@@ -10,20 +11,37 @@ app.engine("handlebars", handlebars());
 app.set("view engine", "handlebars");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(cookieParser());
 
-app.use(express.static(__dirname + "/public")); // refering to my public files
+app.use(
+    cookieSession({
+        secret: "nosecret"
+    })
+);
+
+app.use(express.static(__dirname + "/public"));
 
 app.get("/petition", (req, res) => {
     res.render("main", {
-        // main is the template that i want to render as it has all the body in it
         layout: "layouts"
     });
-}); // url (/petition) in get is whatever i want it to be. this will present my templates in browser "render"
+});
+
+app.get("/thankyou", (req, res) => {
+    signedSession(id);
+    res.render("thankyou-page", {
+        layout: "layouts"
+    });
+});
 
 app.post("/petition", (req, res) => {
-    console.log(req.body);
-    if (!req.body.firstname && !req.body.lastname && !req.body.signature) {
+    if (req.session.signatureId) {
+        res.redirect("/petition");
+        return;
+    }
+
+    if (!req.body.firstname || !req.body.lastname || !req.body.signature) {
         res.render("main", {
             layout: "layouts",
             error: "error"
@@ -34,8 +52,10 @@ app.post("/petition", (req, res) => {
             req.body.lastname,
             req.body.signature
         )
-            .then(function() {
-                res.redirect("/petition");
+            .then(function(id) {
+                console.log("return value from insert new sig", id);
+                req.session.signatureId = id;
+                res.redirect("/thankyou");
             })
             .catch(function(error) {
                 console.log(error);
