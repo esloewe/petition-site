08a5e3ff8b3b1012fd555860865805c2e90f99ff
Estefania Loewenberg <estloewenberg@gmail.com>
2018-03-01 15:31:20 +0100

    fv2.2

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 var petitionSignClick = $("#sign-petition");
 var signatureCanvas = $("#canvas-sign");
+var canvasPositionMobile = signatureCanvas.offset(); //tells me where the canvas is on the phone
 
 var signing = false;
 var canvasDataSave = $("#canvasDataSave");
@@ -17,9 +18,11 @@ signatureCanvas.on("mousedown", e => {
     console.log("this works ");
     signing = true;
     x = e.offsetX; // previous position mouse
-    y = e.offsetY;
+    y = e.offsetY; ///e.touches[0].pageY mobile
 });
 
+e.touches[0].pageX - canvasPosition.left;
+e.touches[0].pageY - canvasPosition.top;
 signatureCanvas.on("mouseup", () => {
     signing = false;
 });
@@ -35,5 +38,29 @@ signatureCanvas.on("mousemove", e => {
         x = e.offsetX; // creates an ongoing recording of the movement
         y = e.offsetY;
         console.log("this works ");
+    }
+});
+
+// MOBILE VERSION
+
+signatureCanvas.on("touchstart", e => {
+    signing = true;
+    x = e.touches[0].pageX - canvasPositionMobile.left;
+    y = e.touches[0].pageY - canvasPositionMobile.top;
+});
+
+signatureCanvas.on("touchmove", e => {
+    if (signing == true) {
+        var context = document.getElementById("canvas-sign").getContext("2d");
+        context.strokeStyle = "#000000";
+        context.lineWidth = 2;
+        context.moveTo(x, y);
+        context.lineTo(
+            e.touches[0].pageX - canvasPositionMobile.left,
+            e.touches[0].pageY - canvasPositionMobile.top
+        );
+        context.stroke();
+        x = e.touches[0].pageX - canvasPositionMobile.left; // creates an ongoing recording of the movement
+        y = e.touches[0].pageY - canvasPositionMobile.top;
     }
 });
